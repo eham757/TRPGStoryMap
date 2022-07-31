b82e652424926b77432765ffb2fa7e6c831beb44
DESKTOP-U8OO4SF\Akash <akash1999@hotmail.nl>
2022-07-31 13:31:38 +0200

    fe init plus leaflet

---
+++ Frontend/TRPGMapArticleFrontEnd/src/app/map/map.component.ts
@@ -0,0 +1,38 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit , AfterViewInit {
+
+  private map: L.Map | undefined;
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  private initMap(){
+    this.map = L.map('map',{
+      crs: L.CRS.Simple,
+      center: [1750,1600], // 840 768
+      minZoom: -2,
+      maxZoom: 2,
+      zoomSnap: 0.1,
+      zoom: 0
+    });
+
+    var imageUrl = '/assets/sigmi_coloured.png',
+    imageBounds: L.LatLngBounds = new L.LatLngBounds(new L.LatLng(0, 0),new L.LatLng(3500, 3200) ) //1680, 1536
+    L.imageOverlay(imageUrl, imageBounds).addTo(this.map);
+  }
+  
+
+}
